f697a7c8a0a48b7b6bfce046ae8c184368487527
Aurko Home <mahmudur.rashid@northsouth.edu>
2022-10-01 22:59:48 +0600

    something

--- try.py
+++ try.py
@@ -1,3 +1,4 @@
+from turtle import color
 import serial # import Serial Library
 import matplotlib.pyplot as plt #import matplotlib library
 from drawnow import *
@@ -10,32 +11,36 @@ ymin = 0
 ymax = 0
 plt.ion()   #Tell matplotlib you want interactive mode to plot live data
 cnt=0
+fig, (ax1, ax2) = plt.subplots(2)
 
 def makeFig(): #Create a function that makes our desired plot   
-    plt.title('Gas Sensor Data')      #Plot the title
-    plt.grid(True)                                  #Turn the grid on
-    plt.ylabel('MQ 7')                            #Set ylabels
-    #plt.ylim(None)
-    plt.plot(sen_one, 'ro-', label='MQ 7')       #plot the temperature
-    plt.legend(loc='upper left')                    #plot the legend
+    ax1.plot(sen_one, color = 'b')       #plot the temperature
+    ax1.grid(True)
+    ax1.set_ylabel('One')
+    ax1.set_title('One Title')
+
+    ax2.plot(sen_two, color = 'r')       #plot the temperature
+    ax2.grid(True)
+    ax2.set_ylabel('Two')
+    ax2.set_title('Two Title')
     
     
-    plt2=plt.twinx()                                #Create a second y axis
-    plt2.plot(sen_two, 'ro-', color='b', label='MQ 2') #plot pressure data
-    plt2.set_ylabel('MQ 2')                    #label second y axis
-    plt2.ticklabel_format(useOffset=False)           #Force matplotlib to NOT autoscale y axis
-    plt2.legend(loc='upper right')                  #plot the legend
+    # plt2=plt.twinx()                                #Create a second y axis
+    # plt2.plot(sen_two, 'ro-', color='b', label='MQ 2') #plot pressure data
+    # plt2.set_ylabel('MQ 2')                    #label second y axis
+    # plt2.ticklabel_format(useOffset=False)           #Force matplotlib to NOT autoscale y axis
+    # plt2.legend(loc='upper right')                  #plot the legend
 
-    plt3=plt.twinx()                                #Create a second y axis
-    plt3.plot(sen_three, 'ro-', color='g', label='MQ 5') #plot pressure data
-    plt3.set_ylabel('MQ 5')                    #label second y axis
-    plt3.ticklabel_format(useOffset=False)           #Force matplotlib to NOT autoscale y axis
-    plt3.legend(loc='lower right')                  #plot the legend
+    # plt3=plt.twinx()                                #Create a second y axis
+    # plt3.plot(sen_three, 'ro-', color='g', label='MQ 5') #plot pressure data
+    # plt3.set_ylabel('MQ 5')                    #label second y axis
+    # plt3.ticklabel_format(useOffset=False)           #Force matplotlib to NOT autoscale y axis
+    # plt3.legend(loc='lower right')                  #plot the legend
 
-    plt4=plt.twinx()                                #Create a second y axis
-    plt4.plot(sen_four, 'ro-', color='y', label='MQ 9') #plot pressure data
-    plt4.ticklabel_format(useOffset=False)           #Force matplotlib to NOT autoscale y axis
-    plt4.legend(loc='lower left')                  #plot the legend
+    # plt4=plt.twinx()                                #Create a second y axis
+    # plt4.plot(sen_four, 'ro-', color='y', label='MQ 9') #plot pressure data
+    # plt4.ticklabel_format(useOffset=False)           #Force matplotlib to NOT autoscale y axis
+    # plt4.legend(loc='lower left')                  #plot the legend
     
 #https://toptechboy.com/python-with-arduino-lesson-11-plotting-and-graphing-live-data-from-arduino-with-matplotlib/
 
@@ -56,8 +61,7 @@ while True: # While loop that loops forever
     sen_two.append(two)                     #Building our sen_two array by appending P readings
     sen_three.append(three)                     #Building our sen_two array by appending P readings
     sen_four.append(four)
-    drawnow(makeFig)                       #Call drawnow to update our live graph
-    plt.pause(.000001)                     #Pause Briefly. Important to keep drawnow from crashing
+    plt.show()                     #Pause Briefly. Important to keep drawnow from crashing
     cnt=cnt+1
     if(cnt>50):                            #If you have 50 or more points, delete the first one from the array
         sen_one.pop(0)                       #This allows us to just see the last 50 data points
